903a82060c7d4dc4ec41b690946ecbfa4ac8aa7f
Navaneethan <navaneethanbio@gmail.com>
2024-01-15 17:56:53 +0800

    Create insert_water.sh

---
+++ Hydration_simulations/insert_water.sh
@@ -0,0 +1,2 @@
+gmx_mpi insert-molecules -f npt_eq_8_5.gro -ci h2o.gro -nmol 340 -try 1000 -rot xyz -o sys.gro 
+# gmx_mpi insert-molecules -f npt_eq_8_5.gro -ci h2o.gro -nmol 681 -try 1000 -rot xyz -o sys.gro 
