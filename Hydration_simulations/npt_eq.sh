07b0004be224e324567717af68ae4ff551f80eca
Navaneethan <navaneethanbio@gmail.com>
2024-01-15 18:04:47 +0800

    Add files via upload

---
+++ Hydration_simulations/npt_eq.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+for i in {1..2}
+do	
+	if [[ $i -eq 1 ]]
+	then
+		gmx_mpi grompp -f npt_eq_$i.mdp -o npt_eq_$i.tpr -c nvt_eq_3.gro -r nvt_eq_3.gro -p topol.top
+		gmx_mpi mdrun -v -deffnm npt_eq_$i #-gpu_id 0 -ntomp 8
+		gmx_mpi trjconv -f npt_eq_$i.trr -s npt_eq_$i.tpr -n System.ndx -o npt_eq_$i.xtc -pbc mol
+		
+	else
+		gmx_mpi grompp -f npt_eq_$i.mdp -o npt_eq_$i.tpr -c npt_eq_$(($i-1)).gro -r npt_eq_$(($i-1)).gro -p topol.top
+		gmx_mpi mdrun -v -deffnm npt_eq_$i  #-gpu_id 0 -ntomp 8
+		gmx_mpi trjconv -f npt_eq_$i.trr -s npt_eq_$i.tpr -n System.ndx -o npt_eq_$i.xtc -pbc mol
+	fi
+done
+
+
+for i in {1..10}
+do	
+	if [[ $i -eq 1 ]]
+	then
+		gmx_mpi grompp -f npt_eq_3.mdp -o npt_eq_3_$i.tpr -c npt_eq_2.gro -r npt_eq_2.gro -p topol.top
+		gmx_mpi mdrun -v -deffnm npt_eq_3_$i #-gpu_id 1 -ntomp 8
+		gmx_mpi trjconv -f npt_eq_3_$i.trr -s npt_eq_3_$i.tpr -n System.ndx -o npt_eq_3_$i.xtc -pbc mol
+		
+	else
+		gmx_mpi grompp -f npt_eq_3.mdp -o npt_eq_3_$i.tpr -c npt_eq_3_$(($i-1)).gro -r npt_eq_3_$(($i-1)).gro -p topol.top
+		gmx_mpi mdrun -v -deffnm npt_eq_3_$i  #-gpu_id 1 -ntomp 8
+		gmx_mpi trjconv -f npt_eq_3_$i.trr -s npt_eq_3_$i.tpr -n System.ndx -o npt_eq_3_$i.xtc -pbc mol
+	fi
+done
+
---
+++ Hydration_simulations/npt_prod.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for i in {2..2}
+do	
+	if [[ $i -eq 1 ]]
+	then
+		gmx_mpi grompp -f npt_prod.mdp -o npt_prod_$i.tpr -c npt_eq_3_10.gro -r npt_eq_3_10.gro -p topol.top
+		gmx_mpi mdrun -v -deffnm npt_prod_$i #-gpu_id 0 -ntomp 8
+		gmx_mpi trjconv -f npt_prod_$i.trr -s npt_prod_$i.tpr -n System.ndx -o npt_prod_$i.xtc -pbc mol
+		
+	else
+		gmx_mpi grompp -f npt_prod.mdp -o npt_prod_$i.tpr -c npt_prod_$(($i-1)).gro -r npt_prod_$(($i-1)).gro -p topol.top
+		gmx_mpi mdrun -v -deffnm npt_prod_$i  #-gpu_id 0 -ntomp 8
+		gmx_mpi trjconv -f npt_prod_$i.trr -s npt_prod_$i.tpr -n System.ndx -o npt_prod_$i.xtc -pbc mol
+	fi
+done
+
