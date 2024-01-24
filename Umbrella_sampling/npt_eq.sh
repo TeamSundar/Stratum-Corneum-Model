1f555d813d046a063081b8a5a227b6fe57846a54
Navaneethan <navaneethanbio@gmail.com>
2024-01-24 15:23:13 +0800

    Delete Umbrella_sampling directory

--- Umbrella_sampling/npt_eq.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gmx_mpi grompp -f npt_eq.mdp -o npt_eq.tpr -c nvt_eq.gro -r nvt_eq.gro -p topol.top -n index.ndx
-gmx_mpi mdrun -v -deffnm npt_eq -gpu_id 1 -ntomp 8
-gmx_mpi trjconv -f npt_eq.trr -s nvt_eq.gro -n System.ndx -o npt_eq.xtc -pbc nojump
-		
-
