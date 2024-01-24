0ace16cc646af2aaf9f43174499b35e9f3c556fc
Navaneethan <navaneethanbio@gmail.com>
2024-01-24 15:22:46 +0800

    Delete Hydration_simulations directory

--- Hydration_simulations/em.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gmx_mpi grompp -f em.mdp -o em.tpr -c sys.gro -r sys.gro -p topol.top
-gmx_mpi mdrun -v -deffnm em #-gpu_id 0 -ntomp 8
-	
--- Hydration_simulations/insert_water.sh
+++
@@ -1,2 +0,0 @@
-gmx_mpi insert-molecules -f npt_eq_8_5.gro -ci h2o.gro -nmol 340 -try 1000 -rot xyz -o sys.gro 
-# gmx_mpi insert-molecules -f npt_eq_8_5.gro -ci h2o.gro -nmol 681 -try 1000 -rot xyz -o sys.gro 
