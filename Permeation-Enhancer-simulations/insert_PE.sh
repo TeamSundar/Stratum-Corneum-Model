7ff135b4858afecfde2dfa6c21e9e4be64ba184c
Navaneethan <navaneethanbio@gmail.com>
2024-01-24 15:23:03 +0800

    Delete Permeation-Enhancer-simulations directory

--- Permeation-Enhancer-simulations/insert_PE.sh
+++
@@ -1,4 +0,0 @@
-gmx_mpi insert-molecules -f npt_eq_8_5.gro -ci eclp.gro -nmol 20 -try 1000 -rot xyz -o sys.gro -scale 0.29
-# gmx_mpi insert-molecules -f npt_eq_8_5.gro -ci eclp.gro -nmol 40 -try 1000 -rot xyz -o sys.gro -scale 0.29
-# gmx_mpi insert-molecules -f npt_eq_8_5.gro -ci limo.gro -nmol 23 -try 1000 -rot xyz -o sys.gro -scale 0.30
-# gmx_mpi insert-molecules -f npt_eq_8_5.gro -ci limo.gro -nmol 45 -try 1000 -rot xyz -o sys.gro -scale 0.30
